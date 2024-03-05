65aaee3648cf89fad257d62048a81fd984fd5cb2
DerFrZocker <git@derfrzocker.de>
2024-03-05 17:37:36 +0100

    Add some missing annotations to the HelpSetRegistration annotation

--- src/main/java/de/dhbw/mwulle/jhelp/netbeans/api/HelpSetRegistration.java
+++ src/main/java/de/dhbw/mwulle/jhelp/netbeans/api/HelpSetRegistration.java
@@ -1,12 +1,21 @@
 package de.dhbw.mwulle.jhelp.netbeans.api;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Used for registration a help set.
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PACKAGE)
 public @interface HelpSetRegistration {
 
     /**
-     * @return the path to the help set file
+     * @return the path to the help set file, relative to the location of this annotation
      */
     String helpSet();
 }
