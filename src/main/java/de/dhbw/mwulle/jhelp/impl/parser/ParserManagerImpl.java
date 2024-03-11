1ec4d2ea3bf9b510cb3a1e85771f9d60083227bb
DerFrZocker <git@derfrzocker.de>
2024-03-11 20:56:37 +0100

    Add own dtd files for helpset and children
    So that the document parser does not fail, if there is no internet access -> Makes it more train friendly

--- src/main/java/de/dhbw/mwulle/jhelp/impl/parser/ParserManagerImpl.java
+++ src/main/java/de/dhbw/mwulle/jhelp/impl/parser/ParserManagerImpl.java
@@ -20,10 +20,15 @@ import de.dhbw.mwulle.jhelp.impl.view.index.IndexViewFactory;
 import de.dhbw.mwulle.jhelp.impl.view.toc.BaseTocItemInfoParser;
 import de.dhbw.mwulle.jhelp.impl.view.toc.TocItemParser;
 import de.dhbw.mwulle.jhelp.impl.view.toc.TocViewFactory;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
@@ -31,11 +36,14 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParserManagerImpl implements ParserManager {
 
     private final HelpSetParser rootHelpSetParser;
     private final DocumentBuilderFactory factory;
+    private final CustomEntityResolver entityResolver = new CustomEntityResolver();
 
     public ParserManagerImpl(HelpSetParser rootHelpSetParser) throws ParserConfigurationException {
         this.rootHelpSetParser = rootHelpSetParser;
@@ -70,7 +78,16 @@ public class ParserManagerImpl implements ParserManager {
 
         rootHelpSetParser.registerChildParser("view", viewHelpSetParser);
 
+        parserManager.registerDTD("-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN", "helpset.dtd");
+        parserManager.registerDTD("-//Sun Microsystems Inc.//DTD JavaHelp Index Version 2.0//EN", "index.dtd");
+        parserManager.registerDTD("-//Sun Microsystems Inc.//DTD JavaHelp Map Version 2.0//EN", "map.dtd");
+        parserManager.registerDTD("-//Sun Microsystems Inc.//DTD JavaHelp TOC Version 2.0//EN", "toc.dtd");
+
         return parserManager;
+    }
+
+    public void registerDTD(String publicId, String dtdFile) {
+        entityResolver.custom.put(publicId, dtdFile);
     }
 
     @Override
@@ -106,11 +123,32 @@ public class ParserManagerImpl implements ParserManager {
     private Document getDocument(URL url) {
         try (InputStream stream = url.openStream()) {
             System.out.println("Getting document for url " + url);
-            Document document = this.factory.newDocumentBuilder().parse(stream);
+            DocumentBuilder documentBuilder = this.factory.newDocumentBuilder();
+            documentBuilder.setEntityResolver(entityResolver);
+            Document document = documentBuilder.parse(stream);
             document.normalize();
             return document;
         } catch (ParserConfigurationException | IOException | SAXException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static class CustomEntityResolver implements EntityResolver {
+
+        private final Map<String, String> custom = new HashMap<>();
+
+        @Override
+        public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+            String dtd = custom.get(publicId);
+
+            System.out.println("Got publixId " + publicId);
+
+            if (dtd == null) {
+                return null;
+            }
+
+            FileObject fileObject = FileUtil.getConfigRoot().getFileObject("Services/JavaHelp/" + dtd);
+            return new InputSource(fileObject.getInputStream());
         }
     }
 }
